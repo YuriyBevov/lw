ea56af8ec82ddb523408f4747123825e93bd7510
Yuriy Bevov <jury19y@mail.ru>
2023-06-12 22:41:49 +0300

    add header, nav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import "./modules/custom_cursor.js";
 import './modules/scrollSmoother.js';
+import './modules/anchor-links.js';
 import './modules/input-number-mask.js';
 import './modules/swiper.js';
